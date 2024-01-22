2ae701557823a38b7d727c4dd77dafc72fcd6f63
Montu-Gohain <leomontugohain@gmail.com>
2024-01-22 12:08:09 +0530

    These changes are made :
    1. Added animation in the home page
    2. Added Roboto Font family in the text

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,17 @@
-import REACT_LOGO from "../public/react_logo.png";
+import REACT_LOGO from "/react_logo.png";
 
 function App() {
   return (
     <>
       <div className="h-screen bg-slate-800">
         <header className="flex justify-center items-center flex-col bg-sky-900 p-3">
-          <h1 className="text-center font-bold text-4xl items-center text-white flex flex-row">
+          <h1 className="text-center font-bold text-4xl items-center text-white flex flex-row font-roboto">
             Journey through React{" "}
             <span>
               <img
                 src={REACT_LOGO}
                 alt="ReactJS Logo"
-                className="w-16 h-auto"
+                className="w-16 h-auto motion-safe:animate-spin-slow"
               />
             </span>
           </h1>
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,14 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        "spin-slow": "spin 3s linear infinite",
+      },
+      fontFamily: {
+        roboto: ["Roboto"],
+      },
+    },
   },
   plugins: [],
 };
